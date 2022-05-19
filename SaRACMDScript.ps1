735e7480ea9174c8177c0a4301bda468212b435a
Ricardo Pacheco <mektos@gmail.com>
2022-05-19 19:56:24 +0100

    Update SaRACMDScript.ps1

--- SaRACMDScript.ps1
+++ SaRACMDScript.ps1
@@ -1,12 +1,13 @@
 
 $title    = 'SaRA EULA'
-$question = 'Do you accept the SaRA application EULA?'
+
 $choices  = '&Yes', '&No'
 $ProgressPreference = 'SilentlyContinue'
 $Destination = $env:TEMP +"\SaRACMD\LatestSaRACMD.zip"
 $Folder =  $env:TEMP +"\SaRACMD\"
 $Temp = $env:TEMP
 $locerr = 'A folder named SaRACMD already exists in ' + $Temp + ', please delete it first!'
+$question = 'Do you accept the SaRA application EULA? Found in $Folder'
 
 
 if (Test-Path -Path $Destination) {
@@ -14,11 +15,7 @@ if (Test-Path -Path $Destination) {
    explorer $env:TEMP
    Read-Host "Press any key to close script..."
 } else {
-Start-Process "https://bing.com"
 
-
-$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-if ($decision -eq 0) {
     Write-Host 'Starting SaRA Download'
     New-Item -Path $Temp -Name "SaRACMD" -ItemType "directory" | Out-Null
 Invoke-WebRequest -uri https://aka.ms/SaRA_CommandLineVersionFiles -OutFile $Destination
@@ -27,6 +24,11 @@ Expand-Archive $Destination -DestinationPath $Folder
 
 sleep 5
 cd $Folder
+SaRAcmd.exe -DisplayEULA > $Folder\SaRAEula.txt
+$decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+if ($decision -eq 0) {
+
+
 
 .\SaRACmd.exe -s ExpertExperienceAdminTask -AcceptEULA
 
