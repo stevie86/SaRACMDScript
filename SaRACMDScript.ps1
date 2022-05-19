58acf16bc0603dc2e976bbfb876201b204992649
Ricardo Pacheco <mektos@gmail.com>
2022-05-19 20:35:27 +0100

    Update SaRACMDScript.ps1
    Updated SaRA EULA to be displayed automatically using Notepad before proceeding.

--- SaRACMDScript.ps1
+++ SaRACMDScript.ps1
@@ -6,7 +6,7 @@ $Destination = $env:TEMP +"\SaRACMD\LatestSaRACMD.zip"
 $Folder =  $env:TEMP +"\SaRACMD\"
 $Temp = $env:TEMP
 $locerr = 'A folder named SaRACMD already exists in ' + $Temp + ', please delete it first!'
-$question = 'Do you accept the SaRA application EULA? Found in $Folder'
+$question = 'Do you accept the SaRA application EULA? Found in ' + $Folder
 
 
 if (Test-Path -Path $Destination) {
@@ -15,21 +15,18 @@ if (Test-Path -Path $Destination) {
    Read-Host "Press any key to close script..."
 } else {
 
-    Write-Host 'Starting SaRA Download'
-    New-Item -Path $Temp -Name "SaRACMD" -ItemType "directory" | Out-Null
-Invoke-WebRequest -uri https://aka.ms/SaRA_CommandLineVersionFiles -OutFile $Destination
+New-Item -Path $Temp -Name "SaRACMD" -ItemType "directory" | Out-Null
+sleep 2
 
-Expand-Archive $Destination -DestinationPath $Folder
-
-sleep 5
 cd $Folder
-
-.\SaRAcmd.exe -DisplayEULA > $Folder\SaRAEula.txt
+Write-Host 'Starting SaRA Download'
+Invoke-WebRequest -uri https://aka.ms/SaRA_CommandLineVersionFiles -OutFile $Destination
+Expand-Archive $Destination -DestinationPath $Folder
+sleep 5
+notepad.exe $Folder\SaraEULA.txt
 
 $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
 if ($decision -eq 0) {
-
-
 
 .\SaRACmd.exe -s ExpertExperienceAdminTask -AcceptEULA
 
